c9187ed00accf51c4352d66afedd9c124364fdef
Lawrence Rivales <lrivales@gmail.com>
2021-12-17 15:46:21 -0500

    refactored user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,86 +3,75 @@ const { User } = require('../models');
 const userController = {
     // get all users
     async getAllUsers(req, res) {
-        let dbUserData = await User.find({}).populate({ path: 'friends' });
-        
-        if (!dbUserData) {
-            res.status(404);
-            return;
+        try {
+            let dbUserData = await User.find({}).populate({ path: 'friends' });
+            res.json(dbUserData)
+        } catch (error) {
+            res.status(404).json(error);
         }
-
-        res.json(dbUserData);
     },
 
     // get user by id
     async getUserById({ params }, res) {
-        let dbUserData = await User.findById(params.id).populate({ path: 'friends' });
-
-        if (!dbUserData) {
-            res.status(404);
-            return;
+        try {
+            let dbUserData = await User.findById(params.id).populate({ path: 'friends' });    
+            res.json(dbUserData);
+        } catch (error) {
+            res.status(404).json(error);
         }
-
-        res.json(dbUserData);
     },
     
     // create user
     async createUser({ body }, res) {
-        let dbUserData = await User.create(body);
-
-        if (!dbUserData) {
-            res.status(404);
-            return;
+        try{
+            let dbUserData = await User.create(body);
+            res.json(dbUserData);
+        } catch (error) {
+            res.status(404).json(error);
         }
-
-        res.json(dbUserData);
     },
 
     // update user
     async updateUser({ params, body }, res) {
-        let dbUserData = await User.findByIdAndUpdate(params.id, body, { new: true, runValidators: true });
-
-        if (!dbUserData) {
-            res.status(404);
-            return;
+        try {
+            let dbUserData = await User.findByIdAndUpdate(params.id, body, { new: true, runValidators: true });
+            res.json(dbUserData);
+        } catch (error) {
+            res.status(404).json(error);
         }
-
-        res.json(dbUserData);
     },
 
     // delete user
     async deleteUser({ params }, res) {
-        let deletedUser = await User.findByIdAndRemove(params.id);
-        
-        if (!deletedUser) {
-            res.status(404);
-            return;
+        try {
+            let deletedUser = await User.findByIdAndRemove(params.id);
+            if (deletedUser) {
+                let dbUserData = await User.updateMany({ friends: params.id }, { $pull: { friends: params.id }});
+                res.json(dbUserData);
+            } else {
+                res.status(404).json({ message: 'User not found!'});
+            }
+        } catch (error) {
+            res.status(404).json(error);
         }
-
-        let dbUserData = await User.updateMany({ friends: params.id }, { $pull: { friends: params.id }});
-
-        res.json(dbUserData);
     },
 
     async addFriend({ params }, res) {
-        let dbUserData = await User.findByIdAndUpdate(params.userId, { $push: { friends: params.friendId }}, { new: true });
-
-        if (!dbUserData) {
-            res.status(404);
-            return;
+        try {
+            let dbUserData = await User.findByIdAndUpdate(params.userId, { $push: { friends: params.friendId }}, { new: true });
+            res.json(dbUserData);
+        } catch (error) {
+            res.status(404).json(error);
         }
-
-        res.json(dbUserData);
     },
 
     async deleteFriend({ params }, res) {
-        let dbUserData = await User.findByIdAndUpdate(params.userId, { $pull: { friends: params.friendId }}, { new: true });
-
-        if (!dbUserData) {
-            res.status(404);
-            return;
+        try {
+            let dbUserData = await User.findByIdAndUpdate(params.userId, { $pull: { friends: params.friendId }}, { new: true });
+            res.json(dbUserData);    
+        } catch (error) {
+            res.status(404).json(error);
         }
-
-        res.json(dbUserData);
     }
 }
 
